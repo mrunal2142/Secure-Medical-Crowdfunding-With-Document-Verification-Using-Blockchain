a7938dc162dc8e444508a430bcb7ba0d95b5bc4b
Mrunal D <mrunal2142@gmail.com>
2023-09-14 13:09:01 +0530

    Crowd Fund Start

--- client/src/pages/CrowdFunding.jsx
+++ client/src/pages/CrowdFunding.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const CrowdFunding = () => {
   return (
-    <div>CrowdFunding</div>
+    <div>CrowdFunding  
+      | starting CrowdFunding - 14/9/2023 | 
+    </div>
   )
 }
 
